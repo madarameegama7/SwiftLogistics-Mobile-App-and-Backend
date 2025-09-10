747c347b1b3900c2e1404e7d0a431a4bf691d120
thevindu02 <dil.thevi@gmail.com>
2025-09-10 21:37:21 +0530

    added ROS service structure

---
+++ backend/services/ROS/ROS/src/main/java/com/swiftlogistics/ROS/RosApplication.java
@@ -0,0 +1,13 @@
+package com.swiftlogistics.ROS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RosApplication.class, args);
+	}
+
+}
---
+++ backend/services/ROS/ROS/src/test/java/com/swiftlogistics/ROS/RosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiftlogistics.ROS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
