b3ede513393272cea076e0b4a1ee72bafaedae77
Madara Meegama <madarameegama7@gmail.com>
2025-09-14 09:30:32 +0530

    Completed warehouse management by nodejs

---
+++ backend/services/wms-mock/wms-mock.js
@@ -0,0 +1,17 @@
+const net = require('net');
+const PORT = 3003;
+
+const server = net.createServer(socket => {
+    console.log('WMS client connected');
+
+    socket.on('data', data => {
+        const msg = data.toString();
+        console.log('Received:', msg);
+        // Send a dummy acknowledgment
+        socket.write(`ACK:${msg}`);
+    });
+
+    socket.on('close', () => console.log('Client disconnected'));
+});
+
+server.listen(PORT, () => console.log(`WMS TCP mock running on port ${PORT}`));
